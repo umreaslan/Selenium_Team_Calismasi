b114ac1a22596165ddd11ede7bf87345a5d2d8d1
unknown <sevdeneda.38@gmail.com>
2023-03-16 01:22:56 +0300

    12.03.23

--- src/test/java/day03_NevztHc/kiwiTest.java
+++ src/test/java/day03_NevztHc/kiwiTest.java
@@ -14,7 +14,7 @@ import org.openqa.selenium.support.ui.Select;
 import java.time.Duration;
 
 public class kiwiTest {
-
+     WebDriver driver;
 // https://www.kiwi.com sayfasina gidin
 // Cookies i reddedin
 // Sag ust kisimdan dil ve para birimi secenegini Turkiye ve TL olarak secin
@@ -27,4 +27,60 @@ public class kiwiTest {
 
 
 
+    @Before
+    public  void  setup(){
+
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        //Select select=new Select(driver.findElement(By.xpath("//select[@data-test='LanguageSelect']")));
+
+    }
+
+    @Test
+    public  void testKiwi() throws InterruptedException {
+
+        driver.get(" https://www.kiwi.com/en/");
+        driver.findElement(By.xpath("(//div[@class='ButtonPrimitiveContent__StyledButtonPrimitiveContent-sc-1nfggrh-0 iFkUjC'])[3]")).click();
+        driver.findElement(By.xpath("//*[text()='TRY']")).click();
+        WebElement language=driver.findElement(By.xpath("//select[@class='Select__StyledSelect-sc-2ijy2y-1 bStxNw']"));
+        Thread.sleep(2);
+        Select dilSecimi=new Select(language);
+        dilSecimi.selectByVisibleText("Türkçe");
+        WebElement currency=driver.findElement(By.xpath("//select[@data-test='CurrencySelect']"));
+        Select paraBirimi=new Select(currency);
+        paraBirimi.selectByValue("try");
+        driver.findElement(By.xpath("//*[text()='Save & continue']"));
+
+
+        Assert.assertTrue(driver.findElement(By.xpath("//div[text()='TRY']")).getText().contains("TRY"));
+       Assert.assertTrue(driver.getCurrentUrl().contains("en"));
+        driver.findElement(By.xpath("//div[@class='ButtonPrimitiveContentChildren__StyledButtonPrimitiveContentChildren-sc-mra4yy-0 dkQFNd'][10]")).click();
+        driver.findElement(By.xpath("//*[text()='Tek Yön']")).click();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    @After
+    public  void tearndown(){
+
+
+        driver.close();
+    }
+
+
 }
+
